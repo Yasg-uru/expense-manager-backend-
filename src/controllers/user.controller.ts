b29d4c3b84bc2c7dd746fb1e2a867636b7a45b7f
yasg-uru <yashpawar12122004@gmail.com>
2024-06-02 16:48:33 +0530

    authentication is completed

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,6 @@
-import User from "../models/User.model";
+import User, { Iuser } from "../models/User.model";
 import { Request, Response } from "express";
-import jwt from "jsonwebtoken";
+
 import bcrypt from "bcryptjs";
 import generatetoken from "../utils/generateToken";
 import sendtoken from "../utils/sendtoken";
@@ -17,3 +17,49 @@ export const registeruser = async (req: Request, res: Response) => {
 
   sendtoken(res, gettoken, 200, user);
 };
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(404).json({
+        message: "please enter email or password",
+      });
+    }
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return res.status(404).json({
+        message: "Invalid credentials",
+      });
+    }
+    const ismatched: boolean = await bcrypt.compare(password, user.password);
+    if (!ismatched) {
+      res.status(404).json({
+        message: "Invalid email or password",
+      });
+    }
+    const gettoken = await generatetoken(user._id);
+    sendtoken(res,gettoken,200,user);
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server Error",
+    });
+  }
+};
+export async function logout(req:Request,res:Response){
+  try {
+     res.cookie("token",null,{
+      expires:new Date(0),
+
+      httpOnly:true
+    })
+    res.status(200).json({
+      success:true,
+      message:"Logged out successfully"
+    })
+  } catch (error) {
+    res.status(500).json({
+      message:"internal server error "
+    })
+  }
+}
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,9 @@
 import  {Router} from "express";
-import { registeruser } from "../controllers/user.controller";
+import { login, logout, registeruser } from "../controllers/user.controller";
 const router:Router=Router();
 router.route('/register').post(registeruser);
+router.route("/login").post(login);
+router.route("/logout").post(logout);
 
 export default router;
 

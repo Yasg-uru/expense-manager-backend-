52b5a1e133dc44805e9956865499216435c50a0c
yasg-uru <yashpawar12122004@gmail.com>
2024-06-09 13:47:04 +0530

    created some changes in budget controller

--- src/routes/Budget.routes.ts
+++ src/routes/Budget.routes.ts
@@ -3,8 +3,8 @@ import { isAuthenticated } from "../middleware/Auth.middleware";
 import { Getyourbudgets, createbudget, deletebudget, generatemonthlyReport, updatebudget } from "../controllers/budget.controllers";
 const budgetrouter = Router();
 budgetrouter.post('/create',isAuthenticated,createbudget);
-budgetrouter.put('/update',isAuthenticated,updatebudget);
-budgetrouter.delete('/delete',isAuthenticated,deletebudget);
+budgetrouter.put('/update/:id',isAuthenticated,updatebudget);
+budgetrouter.delete('/delete/:id',isAuthenticated,deletebudget);
 budgetrouter.get('/monthly',isAuthenticated,generatemonthlyReport);
 budgetrouter.get('/Budgets',isAuthenticated,Getyourbudgets);
 

0e2066cbf6fb7fa4ce98c2481edae73989b5699a
yasg-uru <yashpawar12122004@gmail.com>
2024-06-10 14:03:36 +0530

    correction

--- src/utils/nodemailer.ts
+++ src/utils/nodemailer.ts
@@ -11,18 +11,17 @@ const Transporter = nodemailer.createTransport({
     // pass: 'yash1212204',
   },
 });
-export const sendResetMail=(email:string,token:string)=>{
-    const resetLink = `http://localhost:5173/reset-password/${token}`;
+export const sendResetMail = (email: string, token: string) => {
+  const resetLink = `http://localhost:5173/reset-password/${token}`;
   const mailOptions = {
     to: email,
-    from: 'passwordreset@yourapp.com',
-    subject: 'Password Reset',
+    from: "passwordreset@yourapp.com",
+    subject: "Password Reset",
     text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
            Please click on the following link, or paste this into your browser to complete the process:\n\n
            ${resetLink}\n\n
-           If you did not request this, please ignore this email and your password will remain unchanged.\n`
+           If you did not request this, please ignore this email and your password will remain unchanged.\n`,
   };
 
   return Transporter.sendMail(mailOptions);
-
-}
+};

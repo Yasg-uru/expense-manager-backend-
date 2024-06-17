046f7f3f4907decd3370c7b644b8caff5f9ef347
yasg-uru <yashpawar12122004@gmail.com>
2024-06-17 16:43:26 +0530

    added mongodb atlas database to the project

--- src/config/databaseconfig.ts
+++ src/config/databaseconfig.ts
@@ -3,7 +3,7 @@ const uri: string = "mongodb://127.0.0.1:27017/expense-manager";
 
 async function connectDatabase() {
   try {
-    const response = await mongoose.connect(uri);
+    const response = await mongoose.connect(process.env.MONGO_URL as string);
     console.log(`database is connected with :${response.connection.host}`);
   } catch (error) {
     console.log(`failed to connect with database !`);

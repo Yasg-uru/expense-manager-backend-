618396fcb3364a02ba6778df1c239eeee6dd475b
yasg-uru <yashpawar12122004@gmail.com>
2024-06-17 18:16:26 +0530

    modified database config

--- src/config/databaseconfig.ts
+++ src/config/databaseconfig.ts
@@ -3,7 +3,7 @@ const uri: string = "mongodb://127.0.0.1:27017/expense-manager";
 
 async function connectDatabase() {
   try {
-    const response = await mongoose.connect(process.env.MONGO_URL as string);
+    const response = await mongoose.connect("mongodb+srv://yashpawar12122004:eL0k8jyK5SczlLD4@expense.9zeualh.mongodb.net/?retryWrites=true&w=majority&appName=expense");
     console.log(`database is connected with :${response.connection.host}`);
   } catch (error) {
     console.log(`failed to connect with database !`);
